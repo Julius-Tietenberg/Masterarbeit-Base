54177b0e864d9ccf9b1c881a3b9ae7019fd9bace
Julius Tietenberg <3050167@posteo.net>
2023-07-12 14:30:43 +0200

    buttons seem fixed
    error feedback still missing

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Mirror;
+
+public class ButtonController : NetworkBehaviour
+{
+   
+    // This is invoked to inform all listeners, when a specific button is pressed.
+    public static event Action<int> ButtonWasPressed;
+
+    public GameObject buttonKnob;
+
+    public List<Material> buttonMaterials;
+
+    [SyncVar]
+    public bool buttonPressed;
+    
+    [SyncVar]
+    public int buttonSequenceNumber;
+
+
+    [Command (requiresAuthority = false)]
+    public void CmdPressButton()
+    {
+        Debug.Log("CmdPressButton wird gestartet");
+        if (!buttonPressed)
+        {
+            //play animation (to-do)
+            buttonPressed = true;
+            ButtonWasPressed?.Invoke(buttonSequenceNumber);
+            Debug.Log("Action invoked");
+        }
+        else
+        {
+            //some kind of feedback (to-do)
+            //buttonPressed = false;
+        }
+    }
+
+    public void FeedbackButtonCorrect()
+    {
+        buttonKnob.GetComponent<MeshRenderer>().material = buttonMaterials[1];
+        RpcSetButtonClient(1);
+    }
+    
+    public void FeedbackButtonFalse()
+    {
+        buttonKnob.GetComponent<MeshRenderer>().material = buttonMaterials[2];
+        RpcSetButtonClient(2);
+    }
+    
+    [ClientRpc]
+    public void RpcSetButtonClient(int status)
+    {
+        buttonKnob.GetComponent<MeshRenderer>().material = buttonMaterials[status];
+    }
+    
+    public void ResetButtonState()
+    {
+        buttonPressed = false;
+        buttonKnob.GetComponent<MeshRenderer>().material = buttonMaterials[0];
+        RpcSetButtonClient(0);
+    }
+
+    [ClientRpc]
+    public void RpcPlayButtonAnimation()
+    {
+        //play animation for client
+    }
+    
+    
+}
---
+++ Assets/ButtonPuzzleController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+
+public class ButtonPuzzleController : NetworkBehaviour
+{
+
+    // This list contains all Buttons (in order!).
+    public List<GameObject> buttonsInSequence;
+
+    // The current button that was pressed. Zero if none was pressed yet or when the puzzle resets.
+    [SyncVar]
+    public int currentButtonValue;
+    
+    // Total amount of buttons in the sequence.
+    public int maxButtonValue;
+
+    // Signals if the time to press the follow up button has passed. This is not used for the first button.
+    [SyncVar] 
+    public bool timeExceeded = true;
+    
+    // Will be set to true, once the puzzle is solved. 
+    [SyncVar] 
+    public bool buttonPuzzleSolved;
+
+    public void CheckSequence(int newValue)
+    {
+        if (buttonPuzzleSolved)
+        {
+            return;
+        }
+        if (newValue == 1 && currentButtonValue == 0)
+        {
+            currentButtonValue = newValue;
+            buttonsInSequence[newValue-1].GetComponent<ButtonController>().FeedbackButtonCorrect();
+            timeExceeded = false;
+            Debug.Log("Initial button pressed");
+            StopAllCoroutines();
+            StartCoroutine(WaitAndResetBool(5));
+            //Give some feedback that the button was correct and the next one needs to be pressed within 5secs
+        }
+        else if (newValue == (currentButtonValue + 1) && newValue == maxButtonValue && !timeExceeded)
+        {
+            currentButtonValue = newValue;
+            buttonsInSequence[newValue-1].GetComponent<ButtonController>().FeedbackButtonCorrect();
+            buttonPuzzleSolved = true;
+            Debug.Log("Button Sequence fully solved");
+            // Give feedback that the puzzle is solved and deactivate new inputs
+        }
+        else if (newValue == (currentButtonValue + 1) && !timeExceeded)
+        {
+            currentButtonValue = newValue;
+            buttonsInSequence[newValue-1].GetComponent<ButtonController>().FeedbackButtonCorrect();
+            timeExceeded = false;
+            Debug.Log("Button" + newValue + " was pressed in time");
+            StopAllCoroutines();
+            StartCoroutine(WaitAndResetBool(5));
+            //Give some feedback that the button was correct and the next one needs to be pressed within 5secs
+        }
+        else
+        {
+            buttonsInSequence[newValue-1].GetComponent<ButtonController>().FeedbackButtonFalse();
+            Debug.Log("Wrong Button pressed.");
+            StopAllCoroutines();
+            StartCoroutine(WaitAndResetBool(2));
+            Debug.Log("Buttons will be reset now");
+            
+            foreach (var button in buttonsInSequence)
+            {
+                button.GetComponent<ButtonController>().ResetButtonState();
+                currentButtonValue = 0;
+                //Give some feedback that the puzzle was reset
+            }
+        }
+    }
+
+    private IEnumerator WaitAndResetBool(int secondsToWait)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        timeExceeded = true;
+    }
+
+    private void OnEnable()
+    {
+        ButtonController.ButtonWasPressed += CheckSequence;
+    }
+
+    private void OnDisable()
+    {
+        ButtonController.ButtonWasPressed -= CheckSequence;
+    }
+}
